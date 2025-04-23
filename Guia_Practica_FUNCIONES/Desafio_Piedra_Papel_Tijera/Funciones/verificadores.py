5b92f559b35540665e185e4f3ab065885ac1d50d
Jorge Merep <jorge221810@gmail.com>
2025-04-23 01:59:08 -0300

    Ultimos cambios. Se modificaron algunas funciones y se finalizo el bucle de ganador de partida.

--- Guia_Practica_FUNCIONES/Desafio_Piedra_Papel_Tijera/Funciones/verificadores.py
+++ Guia_Practica_FUNCIONES/Desafio_Piedra_Papel_Tijera/Funciones/verificadores.py
@@ -16,13 +16,13 @@ def verificar_ganador_ronda(jugador: int, maquina: int)-> str:
 
     """
     if (jugador == maquina):
-        resultado = "Empate"
+        resultado_ronda = "Empate"
     elif (jugador == 1 and maquina == 3) or (jugador == 2 and maquina == 1) or (jugador == 3 and maquina == 2):
-        resultado = "Jugador"
+        resultado_ronda = "Jugador"
     else:
-        resultado = "Maquina"
+        resultado_ronda = "Maquina"
 
-    return resultado
+    return resultado_ronda
 
 
 def verificar_estado_partida(aciertos_jugador: int, aciertos_maquina: int, ronda_actual: int)-> bool:
@@ -43,6 +43,13 @@ def verificar_estado_partida(aciertos_jugador: int, aciertos_maquina: int, ronda
         False → Si la partida ha finalizado (porque alguien ganó dos veces seguidas o se jugaron todas las rondas).
     
     """
+    if aciertos_jugador >= 2 or aciertos_maquina >= 2:
+        return False
+    if ronda_actual >= 3:
+        return False
+    
+    return True
+
 
 def verificar_ganador_partida(aciertos_jugador: int, aciertos_maquina: int)-> str:
     """
@@ -60,6 +67,15 @@ def verificar_ganador_partida(aciertos_jugador: int, aciertos_maquina: int)-> st
         "Máquina" → Si la máquina tiene más aciertos.
 
     """
+    if aciertos_jugador > aciertos_maquina:
+        resultado_partida = "Jugador"
+    elif aciertos_jugador < aciertos_maquina:
+        resultado_partida = "Maquina"
+    else:
+        resultado_partida = "Empate"
+    
+    return resultado_partida
+    
 
 def mostrar_elemento(eleccion: int)-> str:
     """
@@ -76,7 +92,6 @@ def mostrar_elemento(eleccion: int)-> str:
         "Tijera" cuando su  elección == 3.
 
     """
-    
 
     match eleccion:
         case 1 : 
@@ -91,38 +106,46 @@ def mostrar_elemento(eleccion: int)-> str:
     return opcion_elegida
 
 
-
-def jugar_piedra_papel_tijera()-> str:
-    """
-    Gestiona toda la lógica del juego, usando las funciones anteriores.
-
-        Flujo de la función:
-        Inicia una partida donde el jugador compite contra la máquina.
-        En cada ronda, el jugador elige una opción y la máquina genera una elección aleatoria.
-        Se determina el ganador de la ronda.
-        Se verifica si la partida continúa o si alguien ha ganado.
-        Al finalizar, la función devuelve quién ganó la partida ("Jugador" o "Máquina").
-
-    """
-    
-
 def validar_entero_entre(numero: int, min: int, max: int)-> int:
     """
     Validar que la opcion ingresada sea numerica.
 
         Args:
 
-        numero: va a ser un int que representa la eleccion del usuario.
+        numero: int que representa la eleccion del usuario.
 
         Returns:
 
-        Un int.
+        int.
 
     """
-
     while not numero.isdigit() or (int (numero)< min or int (numero) > max):
         numero = input('Opcion invalida. Intente nuevamente.')
     
     numero_int = int(numero)
     return numero_int
+
+def mostrar_presentacion():
+
+    """
+    Muestra el menú de presentación del juego. 
+
+        Args:
+        No recibe argumentos.
+
+        Returns:
+        Un string que representa el menú de presentación del juego.
+    """
+
+    menu = \
+        """
+        Bienvenido al  Juego Piedra, Papel o Tijera. Las opciones son:
+
+        1.Piedra.
+        2.Papel.
+        3.Tijera.
+
+        """
+
+    return menu
 
--- Guia_Practica_FUNCIONES/Desafio_Piedra_Papel_Tijera/main.py
+++ Guia_Practica_FUNCIONES/Desafio_Piedra_Papel_Tijera/main.py
@@ -1,25 +1,19 @@
 from Funciones.verificadores import *
 import random
 
-
 #Inicializa el juego
-def mostrar_menu():
-    texto = \
-        """
-        Bienvenido al  Juego Piedra, Papel o Tijera. Las opciones son:
+def jugar_piedra_papel_tijera()-> str:
+    """
+    Gestiona toda la lógica del juego, usando las funciones anteriores.
 
-        1.Piedra.
-        2.Papel.
-        3.Tijera.
+        Flujo de la función:
+        Inicia una partida donde el jugador compite contra la máquina.
+        En cada ronda, el jugador elige una opción y la máquina genera una elección aleatoria.
+        Se determina el ganador de la ronda.
+        Se verifica si la partida continúa o si alguien ha ganado.
+        Al finalizar, la función devuelve quién ganó la partida ("Jugador" o "Máquina").
 
-        """
-    
-    print(texto)
-
-
-
-def aplicacion(): 
-
+    """
     ejecutando = True
 
     contador_rondas = 0
@@ -27,16 +21,62 @@ def aplicacion():
     contador_ganado_maquina = 0
 
     while ejecutando:
-        mostrar_menu()
+        # Llama a una función para mostrar el menu del juego.
+        mostrar_presentacion()
+
+        # Solicita al usuario que ingrese una opcion (1: Piedra, 2: Papel, 3: Tijera).
         ingresar_opcion_usuario = input('Ingrese la opcion deseada: ')
+
+        # Valida que la entrada del usuario sea un número entero entre 1 y 3.
         ingresar_opcion_usuario = validar_entero_entre(ingresar_opcion_usuario, 1 , 3)
+
+        # Genera aleatoriamente la elección de la máquina (1: Piedra, 2: Papel, 3: Tijera).
         eleccion_opcion_maquina = random.randint(1,3)
+
+        # Muestra la elección del usuario.
         print (f'El usuario eligio la opcion: {mostrar_elemento(ingresar_opcion_usuario)}')
+
+        # Muestra la elección de la máquina.
         print (f'La maquina eligio la opcion: {mostrar_elemento(eleccion_opcion_maquina)}')
+
+        # Valida y muestra el resultado de la ronda (Jugador, Máquina o Empate).
         print (f'El resultado de esta ronda es: {verificar_ganador_ronda(ingresar_opcion_usuario, eleccion_opcion_maquina)}')
+
+        # Incrementa el contador de rondas jugadas.
         contador_rondas += 1
-        
-        
 
+         # Si el jugador gana la ronda, incrementa su contador de victorias.
+        if verificar_ganador_ronda(ingresar_opcion_usuario, eleccion_opcion_maquina) == "Jugador":
+            contador_ganado_jugador += 1
 
-aplicacion()
+        # Si la máquina gana la ronda, incrementa su contador de victorias.
+        elif verificar_ganador_ronda(ingresar_opcion_usuario, eleccion_opcion_maquina) == "Maquina":
+            contador_ganado_maquina += 1
+
+        # Muestra el puntaje actual de la partida.    
+        print (f'El puntaje actual es: Jugador {contador_ganado_jugador} - Maquina {contador_ganado_maquina}')
+        
+        # Verifica si la partida debe finalizar (alguien ganó dos veces seguidas o se jugaron 3 rondas).
+        if verificar_estado_partida(contador_ganado_jugador, contador_ganado_maquina, contador_rondas) == False:
+                
+            # Muestra quién ganó la partida (Jugador o Máquina).
+            print (f'El ganador de la partida es: {verificar_ganador_partida(contador_ganado_jugador, contador_ganado_maquina)}')
+            
+            #Se cumplen los requisitos de ganador de partida y se finaliza el juego.
+            ejecutando = False
+
+        # Si la partida no ha terminado, informa que sigue en curso.
+        else:
+            print ('La partida sigue en curso.')
+
+        #La partida debe terminar si el jugador gana 2 veces seguidas o si se juegan 3 rondas.
+        verificar_ganador_partida(contador_ganado_jugador, contador_ganado_maquina)
+
+        #Si la partida no cumple los requisitos, se vuelve a iniciar el ciclo.
+        verificar_estado_partida(contador_ganado_jugador, contador_ganado_maquina, contador_rondas)
+
+        # Informa el ganador de la partida 
+    return verificar_ganador_partida(contador_ganado_jugador, contador_ganado_maquina)
+            
+        
+jugar_piedra_papel_tijera()
